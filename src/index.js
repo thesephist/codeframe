ef1b001a1d211c07d4df62e47708b4d0d2c9563a
Linus Lee <linus@thesephist.com>
2019-04-07 12:03:45 -0700

    Reduce accepted file size limit to 50kb

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.text({
-    limit: '100kb',
+    limit: '50kb',
 }));
 
 const api = require('./api.js');
